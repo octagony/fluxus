6eb499c5553e83c4f2ed4e0e636f76bfdfff45ae
octagony <moctagony@gmail.com>
2023-11-14 18:25:13 +0300

    [FEAT] Setup basic footer

---
+++ app/(landing)/_components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import Logo from "@/components/Logo/Logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="absolute z-10 bottom-0 w-full py-2 px-4 shadow-sm backdrop-blur-sm bg-sky-100/5 flex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button className="text-sky-500" size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button className="text-sky-500" size="sm" variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import { Navbar } from "./_components/Navbar/Navbar";
+import { Footer } from "./_components/Footer/Footer";
 
 export default function LandingLayout({ children }: { children: ReactNode }) {
-	return (
-		<div className="h-full bg-slate-100">
-			<Navbar />
-			<main className="pb-20 bg-slate-100">{children}</main>
-		</div>
-	);
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
 }
